e3265ed82c431acc61b203ca3fda3a6276ee0f40
BdVade <victoraderibigbe03@gmail.com>
2020-06-18 03:20:48 +0200

    almost done with backend and placeholder front end

--- interest_calculator/urls.py
+++ interest_calculator/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from piggyvest.views import InterestCalc
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', InterestCalc.as_view()),
 ]
--- piggyvest/forms.py
+++ piggyvest/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+"""
+Choices = (("cs", "Piggybank CoreSavings"),
+           ("tl", "Target lock"), ("flex", "Flex"), ("flex dollar", "Flex Dollar"))
 
 
+class Plansform(forms.Form):
+    choice = forms.ChoiceField(Choices)
+
+"""
 class Interestform(forms.Form):
-    capital = forms.IntegerField()
-    time = forms.IntegerField()
+    capital = forms.IntegerField(label="capital")
+    time = forms.IntegerField(label="saving time in years")
