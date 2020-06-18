520785e86c70c339e9f0a8a26ccfe16f39f34a20
BdVade <62808643+BdVade@users.noreply.github.com>
2020-06-18 15:53:02 +0200

    Update settings.py
    Added static settings

--- interest_calculator/settings.py
+++ interest_calculator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e@3svs^i4br67%k403wec*=l^!fo!5l@53hf4k!)9@t-(u9*9j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
